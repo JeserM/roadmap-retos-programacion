78a63732fabc4440d6564ec5f00bac3a899991ba
Jeser <jesermartin@gmail.com>
2024-05-09 00:14:29 +0100

    Empezando

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/JeserM.py
@@ -0,0 +1,36 @@
+# 00
+"""SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+/*
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */ """
+
+# Comentario en una linea
+# https://www.python.org/
+
+"""
+Esto es un comentario
+en varias lineas
+"""
+'''
+Esto tambien es un
+comentario en varias
+linea
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+# En python no hay variables, todo puede variar. Pero se puede hacer lo siguiente
+MY_CONSTANT = "Mi constante"
+MY_CONSTANT = "Constante2"
