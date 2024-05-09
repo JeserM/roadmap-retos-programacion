8ff2dabedab825cbbe2f1778e9c8a87876c20a36
Jeser <jesermartin@gmail.com>
2024-05-09 17:58:30 +0100

    #00 - Python

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/JeserM.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/JeserM.py
@@ -31,6 +31,26 @@ linea
 my_variable = "Mi variable"
 my_variable = "Nuevo valor de mi variable"
 
-# En python no hay variables, todo puede variar. Pero se puede hacer lo siguiente
+# En python no hay constantes, todo puede variar. Pero se puede hacer lo siguiente
 MY_CONSTANT = "Mi constante"
 MY_CONSTANT = "Constante2"
+
+# tipos de datos primitivos
+my_int = 1  # Entero
+my_float = 1.5  # Float
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto "
+my_other_string = "Mi otra cadena de texto"
+
+# Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+print("¡Hola, Phyton!")
+
+# Imprime tipo de dato
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
+
+# decir el tipo de dato
+my_int: int = 1
